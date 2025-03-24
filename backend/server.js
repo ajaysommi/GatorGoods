1921bd2f880de5397887214e20f651378e946cd2
Ajay Sommi <ajaysommi7@gmail.com>
2025-03-24 19:49:18 -0400

    bug fixes and improvements.

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,28 @@
 import express from 'express';
-import { MongoURL } from './config';
+import { MongoURL } from './config.js';
+import mongoose from 'mongoose';
 
 const app = express();
 
 //visiting initial splash page
 app.get("/", (req, res) => {
-    res.send("Server is set up and ready!");
+    try {
+        res.send("Server is set up and ready!");
+    } catch (error) {
+        console.error("Server not set up. Error: ", error);
+    }
 })
 
-//current bug, url needs to be http instead of https (cant establish secure connection)
-app.listen(3000, () => {
-    //test message to console indicating connection with port successful.
-    console.log("Test: server started at http://localhost:3000.");
 
-})
+mongoose.connect(MongoURL)
+    .then(() => {
+        console.log("Successfully connected to database.");
+        app.listen(3000, () => {
+            //test message to console indicating connection with port successful.
+            console.log("Test: server started at http://localhost:3000.");
+        
+        }) 
+    })
+    .catch((error) => {
+        console.log(error);
+    });
