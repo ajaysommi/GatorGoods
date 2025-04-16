afd5b94c7f13d0999754aa9f94a8688da01a600c
Aaron Young <froakbeats@gmail.com>
2025-04-16 12:39:25 -0400

    Added controller file and implemented basic functionality for listing routes Not final version, don't merge with master

--- backend/models/Listings.js
+++ backend/models/Listings.js
@@ -3,11 +3,15 @@ import mongoose from 'mongoose'
 const listing_schema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
     },
     cost: {
         type: Number,
-        required: true
+        required: true,
     },
     date: {
         type: Date,
