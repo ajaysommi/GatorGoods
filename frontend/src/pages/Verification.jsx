9988270d3a475fad327eb4eba8cdaecbfc35141b
Ajay Sommi <ajaysommi7@gmail.com>
2025-04-15 20:21:35 -0400

    added routing to listings page with dummy listings

--- frontend/src/pages/Verification.jsx
+++ frontend/src/pages/Verification.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import '../App.css'
 
 function AccountVerification(){
-  const [code, setCode] =useState('')
-  const [message, setMessage] =useState('Enter your UFID for Verification')
+  const [code, setCode] =useState('');
+  const [message, setMessage] =useState('Enter your UFID for Verification');
+  const navigate = useNavigate();
 
-  const handleSubmit =(e) =>{
+  const handleSubmit = async (e) =>{
     e.preventDefault()
     if(code ==='12345678'){
-      setMessage('Verification successful!')
+      setMessage('Verification successful! Redirecting to listings...')
+      //part that actually goes to the next page if successful
+      navigate('/listings');
     } 
     else{
       setMessage('Invalid code. Try Again.')
